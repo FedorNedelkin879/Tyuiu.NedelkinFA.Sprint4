56db114ed4c2b7ce488b7693e00c47af25ae2a98
Федя Неделькин <fffedosss898@gmail.com>
2024-12-04 02:35:02 +0500

    кенгшорп

---
+++ Tyuiu.NedelkinFA.Sprint4.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.NedelkinFA.Sprint4.Task3.V10.Lib
+{
+    public class DataService : ISprint4Task3V10
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            int max = array[2, 0];
+            for (int j = 1; j < columns; j++)
+            {
+                if (array[2, j] > max)
+                {
+                    max = array[2, j];
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ Tyuiu.NedelkinFA.Sprint4.Task3.V10/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.NedelkinFA.Sprint4.Task3.V10.Lib;
+
+int[,] mtrx = new int[5, 5] { { 5, 7, 6, 6, 5 },
+                              { 5, 5, 5, 4, 6 },
+                              { 8, 5, 7, 4, 7 },
+                              { 7, 6, 7, 4, 7 },
+                              { 5, 8, 7, 8, 8 } };
+int rows = mtrx.GetUpperBound(0) + 1;
+int columns = mtrx.Length / rows;
+DataService ds = new DataService();
+int res = ds.Calculate(mtrx);
+Console.WriteLine(res);
+Console.ReadKey();
