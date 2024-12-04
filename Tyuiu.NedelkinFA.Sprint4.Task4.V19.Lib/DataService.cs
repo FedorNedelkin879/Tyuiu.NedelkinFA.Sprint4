fa9bd799fd4a2fe49d06479d3c119d7f3471c1d1
Федя Неделькин <fffedosss898@gmail.com>
2024-12-05 01:24:24 +0500

    dgfhghfhgh

---
+++ Tyuiu.NedelkinFA.Sprint4.Task4.V19.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.NedelkinFA.Sprint4.Task4.V19.Lib
+{
+    public class DataService : ISprint4Task4V19
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            int max = array[2, 0];
+            for (int j = 1; j < columns; j++)
+            {
+                if (array[2, j] > max)
+                {
+                    max = array[2, j];
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ Tyuiu.NedelkinFA.Sprint4.Task4.V19/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.NedelkinFA.Sprint4.Task4.V19.Lib;
+DataService ds = new DataService();
+Console.WriteLine("kolich strok");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("kolich stolb");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] mtrx = new int[rows, columns];
+for  (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.WriteLine($"napishi {i}, {j} element");
+        mtrx[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+Console.WriteLine("\nMassive");
+for (int i = 0;i < columns; i++)
+{
+    for (int j = 0;j < rows; j++)
+    {
+        Console.WriteLine($"{mtrx[i,j]} \t");
+    }
+}
+int res = ds.Calculate(mtrx);
+Console.WriteLine("nul elem:" + res);
+Console.ReadKey();
