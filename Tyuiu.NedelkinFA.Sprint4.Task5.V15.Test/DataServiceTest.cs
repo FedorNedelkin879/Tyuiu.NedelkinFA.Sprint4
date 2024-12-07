27787889a6a7e9571cb1efbc87277bb5b7473c06
Федя Неделькин <fffedosss898@gmail.com>
2024-12-07 22:10:43 +0500

    dfgh

---
+++ Tyuiu.NedelkinFA.Sprint4.Task5.V15.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+using Tyuiu.NedelkinFA.Sprint4.Task5.V15.Lib;
+
+namespace Tyuiu.NedelkinFA.Sprint4.Task5.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void MaxInThirdRow()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5]  { { 5, 7, 6, 6, 5 },
+                                           { 5, 5, 5, 4, 6 },
+                                           { 8, 5, 7, 4, 7 },
+                                           { 7, 6, 7, 4, 7 },
+                                           { 5, 8, 7, 8, 8 } };
+            int res = ds.Calculate(mas2);
+            int wait = 8;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.NedelkinFA.Sprint4.Task5.V15/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.NedelkinFA.Sprint4.Task5.V15.Lib;
+DataService ds  = new DataService();
+Random rnd = new Random();
+Console.WriteLine("kolichestvo strok v massive: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("kolichestvo stolb v massive: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] mtrx = new int[rows, columns];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        mtrx[i, j] = rnd.Next(-1, 1);
+    }
+}
+Console.WriteLine("\nMassive:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{mtrx[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+int res = ds.Calculate(mtrx);
+Console.WriteLine("summa nulevih elementov: " + res);
+Console.ReadKey();
